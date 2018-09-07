ea15b5f9deb0182b669b65fc61a7a9c92a3f610c
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-07 12:39:41 +0300

    Update arole.js

--- commands/arole.js
+++ commands/arole.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");	
 const client = new Discord.Client();
-exports.run = (client, message, args, await) => {
+exports.run = (client, message, args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("```fix\nИзвините, вы не имеете прав на использование этой команды.```");	
     let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);	
    if (!rMember) return errors.cantfindUser(message.channel);	
@@ -9,6 +9,6 @@ exports.run = (client, message, args, await) => {
    let gRole = message.guild.roles.find(`name`, role);	
    if (!gRole) return message.channel.send("```fix\nНе удалось найти эту роль.```");	
     if (rMember.roles.has(gRole.id)) return message.channel.send("```fix\nУказанный участник уже имеет эту роль.```");	
-   await (rMember.addRole(gRole.id));	
-   await message.channel.send('```fix\nРоль успешно добавлена.```');	
+   rMember.addRole(gRole.id);	
+   message.channel.send('```fix\nРоль успешно добавлена.```');	
 }
