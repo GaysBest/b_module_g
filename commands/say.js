fc94abd4fe4edc849dcc5f39e0e0555adb0f86bc
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-07 12:50:24 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");	
 const client = new Discord.Client();
-exports.run = (client, message) => {
+exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["334885914021068800", "477573374705401857"].includes(r.id)) )	
       return message.channel.send("```fix\nИзвините, вы не имеете прав на использование этой команды.```");	
     const sayMessage = args.join(" ");	
