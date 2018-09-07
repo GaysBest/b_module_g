5a31dab2260c9ea3a9136d7e0eb9afb7a9203aec
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-07 12:54:31 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,25 +1,11 @@
 const Discord = require("discord.js");	
 const client = new Discord.Client();
-exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["477573374705401857", "334885914021068800", "477604685721763872"].includes(r.id)) )	
-    return message.channel.send("```fix\nИзвините, вы не имеете прав на использование этой команды.```");	
-      
-   const deleteCount = parseInt(args[0], 10);	
-      
-   if(!deleteCount || deleteCount < 2 || deleteCount > 100)	
-    return message.channel.send("```fix\nПожалуйста, укажите число между 2 и 100.```");	
-      
-   const fetched = message.channel.fetchMessages({limit: deleteCount});	
-  message.channel.bulkDelete(fetched);	
-   const log = message.guild.channels.find('name', 'action-log');	
-  const embed = new Discord.RichEmbed()	
-    .setColor(0x000000)	
-    .setDescription("**Удаление сообщений**")	
-    .addField("Модератор:", `${message.author}`)	
-    .addField("Канал:", message.channel)	
-    .addField("Удалено:", `${args[0]}`)	
-    .setAuthor(message.author.tag, message.author.displayAvatarURL)	
-    .setFooter(`${message.author.id}`)	
-    .setTimestamp();	
-  log.send({ embed });
+exports.run = (client, message, args, tools) => {
+
+  if (isNaN(args[0])) return message.channel.send('**Пожалуйста, укажите правильное число.**');
+  if (args[0] > 100) return message.channel.send('**Пожалуйста, укажите число от 1 до 100.**');
+  message.channel.bulkDelete(args[0])
+    .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete({
+      timeout: 10000
+    }))) 
 }
