cf7ca29b3beb9324d3ae0d3c3490d697e538c885
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-07 13:13:10 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,5 @@ exports.run = (client, message, args, tools) => {
   if (isNaN(args[0])) return message.channel.send('**Пожалуйста, укажите правильное число.**');
   if (args[0] > 100) return message.channel.send('**Пожалуйста, укажите число от 1 до 100.**');
   message.channel.bulkDelete(args[0])
-    .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete({
-      timeout: 100000
-    }))) 
+    .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete('1000')))
 }
