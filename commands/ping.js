39c82872635baa70b2ddefb3f28517de57c07a02
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-07 13:06:47 +0300

    Delete ping.js

--- commands/ping.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");	
-const client = new Discord.Client();
-exports.run = (client, message) => {
-    const m = message.channel.send("<:CHIOBLYAA:487549365217656864>");	
-    m.edit(`:ping_pong: Ваш пинг - ${m.createdTimestamp - message.createdTimestamp}мс. API задержка - ${Math.round(client.ping)}мс.`);	
-}
