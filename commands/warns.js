d5d616d03558766ce28fbaa36e7806ec88de32f2
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-15 12:18:43 +0300

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (client, message, args) => {
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
