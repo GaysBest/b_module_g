c3ea031f8ee6bc9a4411cb9da25f9169fa77111c
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-15 12:45:11 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-exports.run = async (bot, message, args) => {
+exports.run = async (client, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Вы не можете это использовать.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Не удалось найти этого участника.");
