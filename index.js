e78fef5a7f23b75e7f7998e14a7ce9aaebe3db70
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-15 09:55:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ client.on('message', message => {
         }	
   });
 client.on('message', message => {
-    const swearWords = ["Пизд", "пизд", "сук", "Сук", "Пидор", "пидор", " бля ", " Бля ", " хуй ", " Хуй ", "Еблан", "еблан"];	
+    const swearWords = ["Пизд", "пизд", "сук", "Сук", "Пидор", "пидор", " бля ", " Бля ", " хуй ", " Хуй ", "Еблан", "еблан", " еба", " Еба"];	
     if( swearWords.some(word => message.content.includes(word)) ) {	
         if(message.author.id === "477613962989404193") return;
         if(message.member.hasPermission('BAN_MEMBERS')) return;
