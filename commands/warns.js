92dc8ebc8b88d79f4234f31a0b1d1d217e97e5f1
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-15 12:54:27 +0300

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -5,16 +5,7 @@ const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 exports.run = async (client, message, args) => {
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("```fix\nНе удалось найти этого участника.```");
+  if(!wUser) return message.channel.send("```fix\nНе удалось найти этого участника.```");
   let warnlevel = warns[wUser.id].warns;
-  const warns = new Discord.RichEmbed()
-  .setDescription('**Предупреждения**')
-  .addField('Участник:', `<@${wUser.id}>`)
-  .addField('Предупреждения:', `${warnlevel}`)
-  .setFooter(`${message.author.id}`)	
-  .setColor(0x000000)
-  .setAuthor(`BGRU Discord Warns`, message.guild.iconURL)
-  .setTimestamp()
-  message.channel.send({warns});
-
+  message.channel.send(`<@${wUser.id}> имеет ${warnlevel} предупреждений`);
 }
