26518f147558f59e7adadf439dbc20909b08e0e3
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-15 13:26:09 +0300

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args) => {
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.channel.send("```fix\nНе удалось найти этого участника.```");
+  if(!wUser) return message.reply("```fix\nНе удалось найти этого участника.```");
   let warnlevel = warns[wUser.id].warns;
-  message.channel.send(`<@${wUser.id}> имеет ${warnlevel} предупреждений`);
+  message.reply(`<@${wUser.id}> имеет ${warnlevel} предупреждений.`);
 }
