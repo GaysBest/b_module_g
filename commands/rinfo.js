6a35962ece787cb5549c1fcba0f4ae5f402b8990
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-16 20:43:22 +0300

    Update rinfo.js

--- commands/rinfo.js
+++ commands/rinfo.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
-exports.run = async (client,message,args) => {
-  var rol = message.content.split(" ").slice(1).join(" ");
-  let role = message.guild.roles.find("name", `${rol}`)
+const moment = require("moment");
+exports.run = async (client, message, args) => {
+  const rol = message.content.split(" ").slice(1).join(" ");
+  const role = message.guild.roles.find("name", `${rol}`)
   if(!role) return message.channel.send('```fix\nВы должны указать роль.```');
-  var moment = require("moment");
-  var temps = moment(message.createdTimestamp).format("LLLL");
-  var roleinfoEmbed = new Discord.RichEmbed()
+  const temps = moment(message.createdTimestamp).format("LLLL");
+  const roleinfoEmbed = new Discord.RichEmbed()
   .setColor(0x000000)
   .addField('Название:', role.name, true)
   .addField('ID:', role.id, true)
