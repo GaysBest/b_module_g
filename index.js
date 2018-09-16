3e466d1f8a2d1ba12edaedc877b1b4c07c150c7b
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-16 22:25:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
     const embed = new Discord.RichEmbed()	
     .setColor(0x000000)	
     .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL)	
-    .setDescription(`**Сообщение от ${oldMessage.author} изменено**`)	
+    .setDescription(`**Сообщение изменено в ${oldMessage.channel}**`)	
     .addField(`До:`, oldMessage.content)
     .addField(`После:`, newMessage.content)
     .setFooter(`${oldMessage.author.id}`)	
