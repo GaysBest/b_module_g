62cb0d195cddb5ca4f6689c0eee71bd07ca64c79
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-16 21:28:12 +0300

    Update rid.js

--- commands/rid.js
+++ commands/rid.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-exports.run = (client, args, message) => {
+exports.run = async (client, args, message) => {
   const rol = args[0];
   if (!rol) return message.channel.send("```fix\nНеобходимо указать роль.```");	
   const role = message.guild.roles.find("name", `${rol}`);
