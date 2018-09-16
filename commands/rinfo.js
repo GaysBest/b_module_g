78e3c199d0101a1d63e2c3dfc0e2a71acab0e6c1
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-16 20:56:44 +0300

    Update rinfo.js

--- commands/rinfo.js
+++ commands/rinfo.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const superagent = require("superagent");
 const moment = require("moment");
+const temps = moment(message.createdTimestamp).format("LLLL");
 exports.run = async (client, message, args) => {
   const rol = message.content.split(" ").slice(1).join(" ");
   const role = message.guild.roles.find("name", `${rol}`)
   if(!role) return message.channel.send('```fix\nВы должны указать роль.```');
-  const temps = moment(message.createdTimestamp).format("LLLL");
   const roleinfoEmbed = new Discord.RichEmbed()
   .setColor(0x000000)
   .addField('Название:', role.name, true)
@@ -15,7 +15,8 @@ exports.run = async (client, message, args) => {
   .addField('Пингуемая:', role.mentionable ? '\nДа' : 'Нет', true)
   .addField('Дата создания:', moment(role.createdAt).format("LL"), true)
   .setFooter(`Ответ для ${message.author.username}#${message.author.discriminator}`)
-  .setTitle(`${message.author.username}#${message.author.discriminator}`)
+  .setAuthor('BGRU Discord Role Info')
+  .setDescription('**Информация о роли**')
   .setTimestamp()
   message.channel.send(roleinfoEmbed)
 }
