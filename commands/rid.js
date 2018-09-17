152e68703ab39ce855977c83a940a6d3c9ad244e
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-17 14:15:33 +0300

    Update rid.js

--- commands/rid.js
+++ commands/rid.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args) => {
   const rol = args[0];
   if(!rol) return message.channel.send("```fix\nНеобходимо указать роль.```");	
   const role = message.guild.roles.find("name", `${rol}`);
+  if(!role) return message.channel.send('```fix\nНеобходимо указать название существующей роли.```');
   const embed = new Discord.RichEmbed()
   .setColor(0x000000)
   .setDescription(`${role.id}`);
