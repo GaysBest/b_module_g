e44102cac5b128afda7edb61ab40af731918ff76
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-18 19:30:51 +0300

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -14,14 +14,16 @@ exports.run = async (client, message, args) => {
         member = message.author;
     }
     var botUser = member.bot ? "Да": "Нет";
+    var Status = statusList[member.presence.status] || "Оффлайн";
+    var botUser = member.bot ? "Да": "Нет";
     const embed = new Discord.RichEmbed()
     .setThumbnail(member.avatarURL)
     .setColor(0x000000)
     .setFooter(`${member.id}`)
     .setAuthor("BGRU Discord UserInfo")
-    .setDescription(`Участник: ${member.tag}`)
-    .addField("Создан:", `${moment.utc(member.createdAt).format('d/M/ Do YYYY, HH:mm')}`, true)
-    .addField("На сервере с:", `${moment.utc(member.joinedAt).format('d/M/ Do YYYY, HH:mm')}`, true)
+    .setDescription(`**Участник: ${member.tag}**`)
+    .addField("Создан:", `${moment.utc(member.createdAt).format('d/M/YY, HH:mm')}`, true)
+    .addField("На сервере с:", `${moment.utc(member.joinedAt).format('d/M/YY, HH:mm')}`, true)
     .addField("Бот:", botUser, true)
     .setTimestamp()
     .addField("Статус:", member.presence.status + message.author.presence, true);
