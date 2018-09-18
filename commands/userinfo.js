2e75aef82fa43ec1fbfc57116bcf4b1d156457a2
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-18 18:48:26 +0300

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const moment = require("moment");
 exports.run = async (client, message, args) => {
     const statusList = {
         online: "Онлайн",
@@ -22,8 +23,8 @@ exports.run = async (client, message, args) => {
     .setFooter(`${member.id}`)
     .setAuthor("BGRU Discord UserInfo")
     .setDescription("Участник: " + member.tag)
-    .addField("Создан:", member.createdAt.toLocaleString(), true)
-    .addField("На сервере с:", guild.members.get(member.id).joinedAt.toLocaleString(), true)
+    .addField("Создан:", `${moment.utc(user.createdAt).format('dddd/MMMM/YYYY, HH:mm')}`, true)
+    .addField("На сервере с:", `${moment.utc(member.joinedAt).format('dddd/MMMM/YYYY, HH:mm')}`, true)
     .addField("Бот:", botUser, true)
     .setTimestamp()
     .addField("Статус:", Status + activity, true);
