3adfa14a90f07199a9e4cba33b97b14ec05144b6
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-18 18:19:00 +0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 exports.run = async (client, message, args) => {
     let guild = message.channel.guild;
     let embed = new Discord.RichEmbed()
-    .setThumbnail(guild.iconURL())
+    .setThumbnail('https://i.imgur.com/rb5csMg.jpg')
     .setColor(0x000000)
     .addField("Регион:", guild.region, true)
     .addField("Создан:", guild.createdAt.toLocaleString(), true)
