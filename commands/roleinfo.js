61cbe09af74e291bbba3ebe383d8ac4712274111
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-18 18:27:03 +0300

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -4,15 +4,15 @@ exports.run = async (client, message, args) => {
 const guild = message.channel.guild;
 const rol = args[0];
 const role = guild.roles.find("name", `${rol}`);
-if (!role) return message.channel.send("```fix\nНе удалось найти эту роль.```");
+if(!role) return message.channel.send("```fix\nНе удалось найти эту роль.```");
     let embed = new Discord.RichEmbed()
-    .addField("Роль:", `${role.name} - ${role.id}`)
     .setColor(0x000000)
+    .addField("Роль:", `${role.name} - ${role.id}`)
     .addField("Позиция:", role.position, true)
     .addField("Цвет:", role.hexColor, true)
     .addField("Участников:", role.members.size, true)
     .setFooter(`${message.author.id}`)
-    .setDescirption('**Информация о роли**')
+    .setDescription('**Информация о роли**')
     .setAuthor('BGRU Discord RoleInfo')
     .setTimestamp();
     message.channel.send({embed});
