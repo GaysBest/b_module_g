72b4eadc3dca32471d7af8ec750cd6773a5706fb
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-18 19:48:26 +0300

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,20 +7,20 @@ exports.run = async (client, message, args) => {
         idle: "Не активен",
         dnd: "Не беспокоить"
     };
-    const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])) || message.author;
     var botUser = member.bot ? "Да": "Нет";
+    var activity = member.presence.activity !== null ? " - " + member.presence.activity.name: " ";
     var Status = statusList[member.presence.status] || "Оффлайн";
-    var botUser = member.bot ? "Да": "Нет";
     const embed = new Discord.RichEmbed()
     .setThumbnail(member.avatarURL)
     .setColor(0x000000)
     .setFooter(`${member.id}`)
     .setAuthor("BGRU Discord UserInfo")
     .setDescription(`**Участник: ${member.tag}**`)
-    .addField("Создан:", `${moment.utc(member.createdAt).format('D/M/YYYY, HH:mm')}`, true)
-    .addField("На сервере с:", `${moment.utc(member.joinedAt).format('D/M/YYYY, HH:mm')}`, true)
+    .addField("Создан:", `${moment.utc(member.createdAt).format('dddd/MMMM/YYYY, HH:mm')}`, true)
+    .addField("На сервере с:", `${moment.utc(member.joinedAt).format('dddd/MMMM/YYYY, HH:mm')}`, true)
     .addField("Бот:", botUser, true)
     .setTimestamp()
-    .addField("Статус:", member.presence.status + message.author.presence, true);
+    .addField("Статус:", Status + activity, true);
     message.channel.send({embed});
 }
