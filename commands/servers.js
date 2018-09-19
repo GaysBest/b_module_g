5f0669fda5c9b206e86d53047a3ddd43f3dead0c
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-19 17:06:25 +0300

    Create servers.js

---
+++ commands/servers.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");	
+const client = new Discord.Client();
+exports.run = (client, message) => {
+const embed = new Discord.RichEmbed()	
+    .setColor(0x000000)
+    .setThumbnail('https://i.imgur.com/rb5csMg.jpg')
+    .setAuthor('BGRU Game Servers')
+    .setDescription('__Team Fortress 2:__\nBestGames.RU | **Jail #1** - [46.174.52.2:27204](steam://connect/46.174.52.2:27204)\nBestGames.RU | **DeathRun** - [46.174.52.14:27209](steam://connect/46.174.52.14:27209)\nBestGames.RU | **Freak Fortress 2** - [46.174.52.20:27272](steam://connect/46.174.52.20:27272)\nBestGames.RU | **DeathMatch** - [46.174.52.3:27269](steam://connect/46.174.52.3:27269)\n__Garry's Mod:__\nBestGames.RU | **GrandRP** - [212.92.101.38:29015](steam://connect/212.92.101.38:29015)')
+    .setTimestamp()
+    .setFooter(`${message.author.id}`);
+    message.channel.send({embed});
