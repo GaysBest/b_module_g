caa56f8ae0ba951cfc99838f1bffc7a51a8067b0
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-20 19:39:32 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,5 +6,17 @@ exports.run = (client, message, args, tools) => {
   if (isNaN(args[0])) return message.channel.send('**Пожалуйста, укажите правильное число.**');
   if (args[0] > 100) return message.channel.send('**Пожалуйста, укажите число от 1 до 100.**');
   message.channel.bulkDelete(args[0])
-    .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete('1000')))
+    .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete('1000')));
+  const log = message.guild.channels.find('name', 'action-log');	
+  const embed = new Discord.RichEmbed()
+    .setColor(0x000000)	
+    .setAuthor(message.author.tag, message.author.displayAvatarURL)	
+    .setDescription(`**Модерация сообщений**`)	
+    .addField(`Модератор:`, `${message.author}`)
+    .addField(`Канал:`, message.channel)
+    .addField(`Кол-во сообщений:`, `${args[0]}`)
+    .setFooter(`${message.author.id}`)	
+    .setAuthor(message.author.tag, message.author.displayAvatarURL)
+    .setTimestamp()
+  log.send({embed});
 }
