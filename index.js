9486ccb8b748ad017dc644b0137c8cf61d3e0269
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-20 19:04:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ client.on('message', message => {
    client.on('guildMemberAdd', member => {	
       member.guild.channels.get('477572395369234433').send('**' + member.user.toString() + '**, добро пожаловать! Пожалуйста, прочти правила.'); 	
   });	
-  client.on('guildMemberRemove', member => {	
+  client.on('guildMemberRemove', member => {
+      if(!member.bannable) return;
       member.guild.channels.get('477572395369234433').send('**' + member.user.toString() + '**, как жаль, что ты покинул нас!');	
       member.send('Как жаль, что ты покинул нас! Мы всегда рады тебе, возвращайся!\n\nhttps://discord.gg/2EDpngu');
   });
