cc014be412783ba1a5b026da6f3133046597d6b7
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-23 16:24:47 +0300

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -9,6 +9,5 @@ const embed = new Discord.RichEmbed()
     .setTimestamp()
     .setFooter(`${message.author.id}`)
     .addField(`Team Fortress 2`, `BestGames.RU | **Jail #1** - [46.174.52.2:27204](steam://connect/46.174.52.2:27204)\nBestGames.RU | **DeathRun** - [46.174.52.14:27209](steam://connect/46.174.52.14:27209)\nBestGames.RU | **Freak Fortress 2** - [46.174.52.20:27272](steam://connect/46.174.52.20:27272)\nBestGames.RU | **DeathMatch** - [46.174.52.3:27269](steam://connect/46.174.52.3:27269)`)
-    .addField(`Garry's Mod`, `BestGames.RU | **GrandRP** - [212.92.101.38:29015](steam://connect/212.92.101.38:29015)`)
     message.channel.send({embed});
 }
