84bbe952bb778e8591a2ac8b2dec0bab9a75cdd2
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-09-26 15:09:53 +0300

    Update blb.js

--- commands/blb.js
+++ commands/blb.js
@@ -14,13 +14,4 @@ exports.run = async (client, message, args) => {
         if (err) throw err;
         console.log("Connected!");
       });
-      var id = args[0];
-      var time = args[1];
-      var add = "INSERT INTO bluebans (player, ban_lengh, admin, admlvl) VALUES (`${id}`, `${time}`, `STEAM_ID_RCON`, `1`)";
-      con.query(add, function (err, result, client) {
-        if (err) throw err;
-        if (err) return message.channel.send('```fix\nПроизошла ошибка.```');
-        console.log("Domablen CHS");
-        message.channel.send('```fix\nИгрок успешно добавлен в ЧС синей команды.```');
-      });
 }
