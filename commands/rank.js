d5be5bcaf3a006e6c8f5aaa570f7b425b5b2998f
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-11 21:43:14 +0300

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     const bynd = message.guild.roles.find("id", "477611608420515843");
     const abynd = message.guild.roles.find("id", "477612032603062276");
+    const old = message.guild.roles.find("id", "500014542642872333");
     const role = args[0];
     if(role === "ЛАМПА" || role === "лампа" || role === "Лампа") {
         if(!message.member.roles.has(bynd.id)) {
@@ -21,6 +22,16 @@ exports.run = async (client, message, args) => {
         if(message.member.roles.has(abynd.id)) {
             message.member.removeRole(abynd);
             return message.channel.send('```fix\nВы успешно вышли из ранга "МОЛЬ"!```');
+        }
+    }
+    if(role === "ОЛД" || role === "олд" || role === "Олд") {
+        if(!message.member.roles.has(old.id)) {
+        message.member.addRole(old);
+        return message.channel.send('```fix\nВы успешно присоединились к рангу "ОЛД"!```');
+        }
+        if(message.member.roles.has(old.id)) {
+        message.member.removeRole(old);
+        return message.channel.send('```fix\nВы успешно вышли из ранга "ОЛД"!```');
         }
     }
 }
