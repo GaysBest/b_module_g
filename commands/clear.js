6e96a392f812ba952646200d6407862889dd7031
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-12 20:23:33 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");	
 const client = new Discord.Client();
 exports.run = (client, message, args, tools) => {
-  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('```fix\nИзвините, вы не можете использовать эту команду.```')
-
+  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('```fix\nИзвините, вы не можете использовать эту команду.```');
+  message.delete();
   if (isNaN(args[0])) return message.channel.send('**Пожалуйста, укажите правильное число.**');
   if (args[0] > 100) return message.channel.send('**Пожалуйста, укажите число от 1 до 100.**');
-  message.delete();
   message.channel.bulkDelete(args[0])
     .then(messages => message.channel.send(`**Успешно удалено \`${messages.size}/${args[0]}\` сообщений.**`).then(msg => msg.delete('1000')));
   const log = message.guild.channels.find('name', 'action-log');	
