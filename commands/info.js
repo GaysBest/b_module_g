a9fd52a84e40728d1865bc82a9bc24eacd323b0b
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 19:54:07 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,33 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
 const server = args[0];
-const jailembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
-    .setTitle(`BestGames.RU | Jail #1`)
-    .setColor(0x000000);
-const drembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
-    .setTitle(`BestGames.RU | DeathRun`)
-    .setColor(0x000000);
-const ff2embed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
-    .setTitle(`BestGames.RU | Freak Fortress 2`)
-    .setColor(0x000000);
-const dmembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
-    .setTitle(`BestGames.RU | DeathMatch`)
-    .setColor(0x000000);
-    
+var jailip = "46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var drip = "46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var ff2ip = "46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var dmip = "46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
     if(server === "jail") {
-        message.channel.send(jailembed);
+        message.channel.send(`http://cache.gametracker.com/server_info/${jailip}`);
     }
     if(server === "dr") {
-        message.channel.send(drembed);
+        message.channel.send(`http://cache.gametracker.com/server_info/${drip}`);
     }
     if(server === "ff2") {
-        message.channel.send(ff2embed);
+        message.channel.send(`http://cache.gametracker.com/server_info/${ff2ip}`);
     }
     if(server === "dm") {
-        message.channel.send(dmembed);
+        message.channel.send(`http://cache.gametracker.com/server_info/${dmip}`);
     }
 }
