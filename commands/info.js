cd8a4af050314744462199a8d6fcaa3c171b30c3
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 19:34:45 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,7 +7,7 @@ var drip = "46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png
 var ff2ip = "46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
 var dmip = "46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
     if(server === "jail") {
-        message.channel.send(`https://cache.gametracker.com/server_info/${jailip}`);
+        message.channel.send(`http://cache.gametracker.com/server_info/${jailip}`);
     }
     if(server === "dr") {
         message.channel.send(`https://cache.gametracker.com/server_info/${drip}`);
