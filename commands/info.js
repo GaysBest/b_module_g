c36f7dd5512f4063c24b6327e404c32cf563bedd
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 21:52:37 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
 const server = args[0];
-var jailip = "http://gametracker.com/server_info/46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var drip = "http://gametracker.com/server_info/46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var ff2ip = "http://gametracker.com/server_info/46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var dmip = "http://gametracker.com/server_info/46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var jailip = "https://gametracker.com/server_info/46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var drip = "https://gametracker.com/server_info/46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var ff2ip = "https://gametracker.com/server_info/46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var dmip = "https://gametracker.com/server_info/46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
     if(server === "jail") {
         message.channel.send(jailip);
     }
