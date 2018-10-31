ed477faa8566942470fd5e4e8b96fc60d548c36a
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 19:49:58 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,18 +2,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
 const server = args[0];
-var jailip = "46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var drip = "46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var ff2ip = "46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var dmip = "46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
 const jailembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/${jailip}`);
+    .setImage(`http://cache.gametracker.com/server_info/46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
+    .setColor(0x000000);
 const drembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/${drip}`);
+    .setImage(`http://cache.gametracker.com/server_info/46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
+    .setColor(0x000000);
 const ff2embed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/${ff2ip}`);
+    .setImage(`http://cache.gametracker.com/server_info/46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
+    .setColor(0x000000);
 const dmembed = new Discord.RichEmbed()
-    .setImage(`http://cache.gametracker.com/server_info/${dmip}`);
+    .setImage(`http://cache.gametracker.com/server_info/46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png`)
+    .setColor(0x000000);
+    
     if(server === "jail") {
         message.channel.send(jailembed);
     }
