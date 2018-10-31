6165e44bb7866a71e8617c6fbbfbbccd86be2545
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 20:01:11 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,15 +7,15 @@ var drip = "46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png
 var ff2ip = "46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
 var dmip = "46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
     if(server === "jail") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${jailip}`);
+        message.channel.send(`http://cache.gametracker.com/server_info/${jailip}.Date.now()`);
     }
     if(server === "dr") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${drip}`);
+        message.channel.send(`http://cache.gametracker.com/server_info/${drip}.Date.now()`);
     }
     if(server === "ff2") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${ff2ip}`);
+        message.channel.send(`http://cache.gametracker.com/server_info/${ff2ip}.Date.now()`);
     }
     if(server === "dm") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${dmip}`);
+        message.channel.send(`http://cache.gametracker.com/server_info/${dmip}.Date.now()`);
     }
 }
