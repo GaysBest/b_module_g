870ded2027783588fda7e856adc42bfe0b69e74a
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-10-31 20:05:47 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,20 +2,24 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
 const server = args[0];
-var jailip = "46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var drip = "46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var ff2ip = "46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
-var dmip = "46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var jailip = "http://cache.gametracker.com/server_info/46.174.52.2:27204/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var drip = "http://cache.gametracker.com/server_info/46.174.52.14:27209/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var ff2ip = "http://cache.gametracker.com/server_info/46.174.52.20:27272/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+var dmip = "http://cache.gametracker.com/server_info/46.174.52.3:27269/b_160_400_0_FFFFFF_FFFFFF_FFFFFF_0D0E12_0_1_0.png";
+const jurl = `${jailip}=forDiscord=${Date.now()}`
+const drurl = `${drip}=forDiscord=${Date.now()}`
+const ff2url = `${ff2ip}=forDiscord=${Date.now()}`
+const dmurl = `${dmip}=forDiscord=${Date.now()}`
     if(server === "jail") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${jailip}.Date.now()`);
+        message.channel.send(jurl);
     }
     if(server === "dr") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${drip}.Date.now()`);
+        message.channel.send(drurl);
     }
     if(server === "ff2") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${ff2ip}.Date.now()`);
+        message.channel.send(ff2url);
     }
     if(server === "dm") {
-        message.channel.send(`http://cache.gametracker.com/server_info/${dmip}.Date.now()`);
+        message.channel.send(dmurl);
     }
 }
