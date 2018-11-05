868c0e440c03b0e544d281b48394404c77b5c892
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-11-05 15:04:01 +0300

    Update sinfo.js

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-exports.run = async (client, message, args) => {
+exports.run = (client, message, args) => {
 var serv = args[0];
 if(serv === "jail" || serv === "1") {
   const jembed = new Discord.RichEmbed()
