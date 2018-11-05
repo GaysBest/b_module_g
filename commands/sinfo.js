a7a681b09370500efb4a429eef84358bdfb493fe
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-11-05 15:15:28 +0300

    Update sinfo.js

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -3,9 +3,12 @@ const client = new Discord.Client();
 exports.run = (client, message, args) => {
 var serv = args[0];
 if(serv === "jail" || serv === "1") {
-  const jembed = new Discord.RichEmbed()
-    .setColor("#000000")
-    .setImage(`http://img.myarena.ru/8789/560.png`);
-  message.channel.send({ jembed });
+  message.channel.send(`http://img.myarena.ru/8789/560.png`);
+}
+if(serv === "dr" || serv === "2") {
+  message.channel.send(`http://img.myarena.ru/8237/560.png`);
+}
+if(serv === "ff2" || serv === "3") {
+  message.channel.send(`http://img.myarena.ru/4053/560.png`);
 }
 }
