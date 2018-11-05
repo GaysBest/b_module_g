4e24f516d6d4596b6c9b006b9f05097f15d05021
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-11-05 14:57:15 +0300

    Update sinfo.js

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -4,8 +4,8 @@ exports.run = async (client, message, args) => {
 var serv = args[0];
 if(serv === "jail" || serv === "1") {
   const jembed = new Discord.RichEmbed()
-    .setColor(0x000000)
-    .setImage('https://img.myarena.ru/8789/560.png');
+    .setColor("#000000")
+    .setImage("http://img.myarena.ru/8789/560.png");
   message.channel.send(jembed);
 }
 }
