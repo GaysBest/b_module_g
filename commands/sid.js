d99c41fa79abf84fefdb03814f657d34ce8eee00
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-11-15 17:02:45 +0300

    Update sid.js

--- commands/sid.js
+++ commands/sid.js
@@ -7,12 +7,11 @@ exports.run = async (client, message, args) => {
             return message.channel.send("```fix\nНеобходимо указать SteamID.```");
             }
         var sid = new SteamID(id);
-        var forlink = JSON.stringify(sid.getSteamID64())
-        var embedlink = 'https://steamcommunity.com/id/${String(forlink)}/'
+        var link = 'https://steamcommunity.com/id/${sid.getSteamID64())}/'
         try {
             var steam3 = new Discord.RichEmbed()
             .setAuthor("BGRU Discord SteamID Finder")
-            .setDescription(embedlink)
+            .setDescription(link)
             .addField("SteamID:", sid.getSteam2RenderedID())
             .addField("SteamID3:", sid.getSteam3RenderedID())
             .addField("SteamID64:", sid.getSteamID64())
