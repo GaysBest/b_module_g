7faf7455e90bb74de036e7ce60c42e877da29b5e
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-11-24 18:16:10 +0300

    Update sinfo.js

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -1,23 +1,37 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const request = require('request');
+const Discord = require('discord.js');
+
 exports.run = (client, message, args) => {
-var serv = args[0];
-var embed = new Discord.RichEmbed()
-  .setColor(0x000000)
-  .setImage(`http://img.myarena.ru/8789/560.png`);
-var embed2 = new Discord.RichEmbed()
-  .setColor(0x000000)
-  .setImage(`http://img.myarena.ru/8237/560.png`);
-var embed3 = new Discord.RichEmbed()
-  .setColor(0x000000)
-  .setImage(`http://img.myarena.ru/4053/560.png`);
-if(serv === "jail" || serv === "1") {
-  message.channel.send({ embed });
-}
-if(serv === "dr" || serv === "2") {
-  message.channel.send({ embed2 });
-}
-if(serv === "ff2" || serv === "3") {
-  message.channel.send({ embed3 });
-}
-}
+    const ip = args.join(" ");
+    if ip == 'jail' {
+      ip = '46.174.52.2:27204'
+    }
+    if ip == 'dr' {
+      ip = '46.174.52.14:27209'
+    }
+    if ip == 'ff2' {
+      ip = '46.174.52.20:27272'
+    }
+    if ip == 'dm' {
+      ip = '46.174.52.3:27269'
+    }
+    if(!ip) return message.channel.send("```fix\nУкажите IP.```")
+  request(`https://use.gameapis.net/source/query/info/${ip}`, {json: true, time: true}, function (error, response, body) {
+    if (error) return exit(error, message);
+    try {
+    const status = new Discord.RichEmbed()
+    .setTitle(`${body.name}`)
+    .addField(`Карта:`, body.map, true)
+    .addField('Количество игроков:', `${body.players.online}/${body.players.max}`, true)
+    .setColor(0x000000)
+    .setDescription(body.join)
+    .setTimestamp()
+    .setFooter(`IP | ${body.hostname}:${body.port}`)
+    .addField("Защищен паролем:", body.password_protected ? "Да" : "Нет", true)
+    .setThumbnail("https://cdn.discordapp.com/attachments/507506673167106048/515901780509458432/latest.png");
+    message.channel.send(status);
+    } catch(e) {
+        message.channel.send("```fix\nУкажите действительный IP и Порт.```");
+    }
+  });
+};
