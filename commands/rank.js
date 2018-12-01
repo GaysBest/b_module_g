24d3d3eaca1dc99f5eb35372bc8b43c49815d182
GaysBest <42907578+GaysBest@users.noreply.github.com>
2018-12-01 16:20:06 +0300

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -4,24 +4,24 @@ exports.run = async (client, message, args) => {
     const abynd = message.guild.roles.find("id", "477612032603062276");
     const old = message.guild.roles.find("id", "500014542642872333");
     const role = args[0];
-    if(role === "ДВОЕЧНИК" || role === "двоечник" || role === "Двоечник") {
+    if(role === "СО" || role === "со" || role === "Со") {
         if(!message.member.roles.has(bynd.id)) {
         message.member.addRole(bynd);
-        return message.channel.send('```fix\nВы успешно присоединились к рангу "Двоечник".```');
+        return message.channel.send('```fix\nВы успешно присоединились к рангу "Со смыслом".```');
         }
         if(message.member.roles.has(bynd.id)) {
         message.member.removeRole(bynd);
-        return message.channel.send('```fix\nВы успешно вышли из ранга "Двоечник".```');
+        return message.channel.send('```fix\nВы успешно вышли из ранга "Со смыслом".```');
         }
     }
-    if(role === "ОТЛИЧНИК" || role === "Отличник" || role === "отличник") {
+    if(role === "БЕЗ" || role === "без" || role === "Без") {
         if(!message.member.roles.has(abynd.id)) {
             message.member.addRole(abynd);
-            return message.channel.send('```fix\nВы успешно присоединились к рангу "Отличник".```');
+            return message.channel.send('```fix\nВы успешно присоединились к рангу "Без смысла".```');
         }
         if(message.member.roles.has(abynd.id)) {
             message.member.removeRole(abynd);
-            return message.channel.send('```fix\nВы успешно вышли из ранга "Отличник".```');
+            return message.channel.send('```fix\nВы успешно вышли из ранга "Без смысла".```');
         }
     }
     if(role === "ОЛД" || role === "олд" || role === "Олд") {
