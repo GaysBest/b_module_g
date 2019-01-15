1e61d6a8a85324f3ae13de3f551f67631b1691cd
GaysBest <42907578+GaysBest@users.noreply.github.com>
2019-01-15 15:52:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
-    if(newMember.id === "323764173995638784" || newMember.id === "285457951248875521" || newMember.id === "281424024523898880" || newMember.id === "369182213339873280" || newMember.id === "358565534293426187") return;
+    if(newMember.id === "323764173995638784" || newMember.id === "285457951248875521" || newMember.id === "281424024523898880" || newMember.id === "369182213339873280" || newMember.id === "358565534293426187" || newMember.id === "339096944808820766") return;
     if(newMember.roles.some(r=>["509034732684705814"].includes(r.id)) )
         return newMember.removeRole(`509034732684705814`);
 });
